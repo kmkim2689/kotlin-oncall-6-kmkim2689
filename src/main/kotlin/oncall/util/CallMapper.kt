5014515121a2e674a89102bd104e70accb14bfef
Kim Gimun <kmkim7575@gmail.com>
2023-12-16 14:19:19 +0900

    feat: 시작 요일이 일~토 중 하나로 입력되지 않았을 경우 예외 발생시키는 기능 추가

--- src/main/kotlin/oncall/util/CallMapper.kt
+++ src/main/kotlin/oncall/util/CallMapper.kt
@@ -7,12 +7,17 @@ import oncall.constant.Constants.SUNDAY
 import oncall.constant.Constants.THURSDAY
 import oncall.constant.Constants.TUESDAY
 import oncall.constant.Constants.WEDNESDAY
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 val months = (1..12).map { it.toString() }.toList()
 val days = listOf(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY)
 
 fun String.toValidatedMonth(): String {
     if (this !in months) throw IllegalArgumentException()
+    return this
+}
+
+fun String.toValidatedDay(): String {
+    if (this !in days) throw IllegalArgumentException()
     return this
 }
