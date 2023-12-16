b16c477acafee0c08ea8d4028fb71f9284ef364f
Kim Gimun <kmkim7575@gmail.com>
2023-12-16 16:20:31 +0900

    fix: DayOfWeekInfo에 해당 요일이 1일이라면 주말이 되는 인덱스 리스트 추가

--- src/main/kotlin/oncall/domain/model/DayOfWeekInfo.kt
+++ src/main/kotlin/oncall/domain/model/DayOfWeekInfo.kt
@@ -10,13 +10,20 @@ import oncall.constant.Constants.DOW_WEDNESDAY
 
 enum class DayOfWeekInfo(
     val dayOfWeek: String,
-    val isWeekend: Boolean
+    val isWeekend: Boolean,
+    val weekendRemainder: List<Int>
 ) {
-    SUNDAY(DOW_SUNDAY, true),
-    MONDAY(DOW_MONDAY, false),
-    TUESDAY(DOW_TUESDAY, false),
-    WEDNESDAY(DOW_WEDNESDAY, false),
-    THURSDAY(DOW_THURSDAY, false),
-    FRIDAY(DOW_FRIDAY, false),
-    SATURDAY(DOW_SATURDAY, true),
+    SUNDAY(DOW_SUNDAY, true, listOf(0, 1)),
+    MONDAY(DOW_MONDAY, false, listOf(0, 1)),
+    TUESDAY(DOW_TUESDAY, false, listOf(1, 2)),
+    WEDNESDAY(DOW_WEDNESDAY, false, listOf(2, 3)),
+    THURSDAY(DOW_THURSDAY, false, listOf(3, 4)),
+    FRIDAY(DOW_FRIDAY, false, listOf(2, 3)),
+    SATURDAY(DOW_SATURDAY, true, listOf(1, 2));
+
+    companion object {
+        fun getDayOfWeekByName(name: String): DayOfWeekInfo? {
+            return entries.find { it.dayOfWeek == name }
+        }
+    }
 }
