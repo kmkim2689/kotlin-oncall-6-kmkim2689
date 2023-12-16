2b26ff3e0e546788cb8abf0b7408c76070642315
Kim Gimun <kmkim7575@gmail.com>
2023-12-16 14:23:29 +0900

    feat: 입력받은 월과 시작일의 리스트의 사이즈가 유효하지 않을 시 예외 발생시키는 기능 추가

---
+++ src/main/kotlin/oncall/constant/ExceptionMessages.kt
@@ -0,0 +1,7 @@
+package oncall.constant
+
+object ExceptionMessages {
+    const val ERROR = "[ERROR]"
+
+    const val EXCEPTION_INVALID_VALUE = "유효하지 않은 입력 값입니다. 다시 입력해 주세요."
+}
--- src/main/kotlin/oncall/util/CallMapper.kt
+++ src/main/kotlin/oncall/util/CallMapper.kt
@@ -7,17 +7,22 @@ import oncall.constant.Constants.SUNDAY
 import oncall.constant.Constants.THURSDAY
 import oncall.constant.Constants.TUESDAY
 import oncall.constant.Constants.WEDNESDAY
+import oncall.constant.ExceptionMessages.EXCEPTION_INVALID_VALUE
 import kotlin.IllegalArgumentException
 
 val months = (1..12).map { it.toString() }.toList()
 val days = listOf(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY)
 
+fun List<String>.isStartDayInputSizeValid() {
+    if (size != 2) throw java.lang.IllegalArgumentException(EXCEPTION_INVALID_VALUE)
+}
+
 fun String.toValidatedMonth(): String {
-    if (this !in months) throw IllegalArgumentException()
+    if (this !in months) throw IllegalArgumentException(EXCEPTION_INVALID_VALUE)
     return this
 }
 
 fun String.toValidatedDay(): String {
-    if (this !in days) throw IllegalArgumentException()
+    if (this !in days) throw IllegalArgumentException(EXCEPTION_INVALID_VALUE)
     return this
 }
