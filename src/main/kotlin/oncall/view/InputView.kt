b147b3de73c74c267a229362b95cdcfb3718478e
Kim Gimun <kmkim7575@gmail.com>
2023-12-16 14:43:57 +0900

    feat: 평일 비상근무 사원들을 입력받고 유효한 형태의 리스트로 변환하는 기능 추가

--- src/main/kotlin/oncall/view/InputView.kt
+++ src/main/kotlin/oncall/view/InputView.kt
@@ -5,7 +5,10 @@ import oncall.constant.Constants.SEPARATOR_INPUT
 import oncall.constant.ExceptionMessages.ERROR
 import oncall.constant.ExceptionMessages.EXCEPTION_INVALID_VALUE
 import oncall.constant.StepMessages.STEP_INPUT_START_DAY
+import oncall.constant.StepMessages.STEP_INPUT_WEEKDAY_CALL
+import oncall.constant.StepMessages.STEP_INPUT_WEEKEND_CALL
 import oncall.util.isStartDayInputValid
+import oncall.util.isWeekdayCallInputValid
 
 object InputView {
     fun getCallStartInfo(): List<String> = try {
@@ -18,5 +21,17 @@ object InputView {
         getCallStartInfo()
     }
 
+    fun getCallMembers(): List<List<String>> = try {
+        print(STEP_INPUT_WEEKDAY_CALL)
+        val weekdayCallMembers = Console.readLine().trim().split(SEPARATOR_INPUT)
+        weekdayCallMembers.isWeekdayCallInputValid()
 
+//        print(STEP_INPUT_WEEKEND_CALL)
+//        val weekendCallMembers = Console.readLine().trim().split(SEPARATOR_INPUT)
+//
+//
+    } catch (e: IllegalArgumentException) {
+        println("$ERROR ${e.message}")
+        getCallMembers()
+    }
 }
