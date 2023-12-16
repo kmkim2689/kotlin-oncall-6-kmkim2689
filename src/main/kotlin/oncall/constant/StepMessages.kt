3337100a5c9cbffede8bf5978844150fc3eee61a
Kim Gimun <kmkim7575@gmail.com>
2023-12-16 18:25:18 +0900

    fix: OutputView에 대한 상수 추가

--- src/main/kotlin/oncall/constant/StepMessages.kt
+++ src/main/kotlin/oncall/constant/StepMessages.kt
@@ -4,4 +4,10 @@ object StepMessages {
     const val STEP_INPUT_START_DAY = "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
     const val STEP_INPUT_WEEKDAY_CALL = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
     const val STEP_INPUT_WEEKEND_CALL = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
+
+    const val STEP_MONTH = "월"
+    const val STEP_DAY_NUM = "일"
+
+    const val STEP_HOLIDAY = "(휴일)"
+    const val STEP_NONE = ""
 }
--- src/main/kotlin/oncall/view/OutputView.kt
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -1,18 +1,22 @@
 package oncall.view
 
+import oncall.constant.StepMessages.STEP_DAY_NUM
+import oncall.constant.StepMessages.STEP_HOLIDAY
+import oncall.constant.StepMessages.STEP_MONTH
+import oncall.constant.StepMessages.STEP_NONE
 import oncall.domain.model.CallInfo
 
 object OutputView {
     fun printResult(callResult: List<CallInfo>) {
-        var result = ""
+        var result = STEP_NONE
         callResult.onEach {
-            result += "${it.month}월 ${it.dayOfMonth}일 ${it.dayOfWeek}${getHolidayMark(it.isHoliday)} ${it.worker}\n"
+            result += "${it.month}$STEP_MONTH ${it.dayOfMonth}$STEP_DAY_NUM ${it.dayOfWeek}${getHolidayMark(it.isHoliday)} ${it.worker}\n"
         }
         println(result)
     }
 
     fun getHolidayMark(isHoliday: Boolean) = when (isHoliday) {
-        true -> "(휴일)"
-        false -> ""
+        true -> STEP_HOLIDAY
+        false -> STEP_NONE
     }
 }
